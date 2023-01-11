84aaf323f41d02fe0a290c8b9874cf682bb20721
Mindaugas Ulonas <mulonas@Mindaugass-MacBook-Air.local>
2023-01-12 00:39:40 +0200

    fixing bug

--- magicbrainlearning2/src/Components/FaceDetect/FaceDetect.js
+++ magicbrainlearning2/src/Components/FaceDetect/FaceDetect.js
@@ -5,7 +5,7 @@ const FaceDetect = ({ imageURL, setFaceBox }) => {
     return(
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img id='inputimage' className='center pa4' width='500px' height='auto' alt='' src={imageURL}></img>
+                <img id='inputimage' width='500px' height='auto' alt='' src={imageURL}></img>
                 <div className='bounding-box' style={{top: setFaceBox.topRow, right: setFaceBox.rightCol, bottom: setFaceBox.bottomRow, left: setFaceBox.leftCol}}></div>
             </div>
         </div>
